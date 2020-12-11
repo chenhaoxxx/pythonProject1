d5bc0e4af21664c2bf69aaa6d2c14e0fc30c25b5
chenhaoxxx <774229975@qq.com>
2020-12-11 16:34:46 +0800

    homework

---
+++ prac 1/loop.py
@@ -0,0 +1,4 @@
+user_input = int(input("number_of_star:"))
+for i in range(user_input):
+    print('*',end=' ')
+print()
--- prac 4/quick_picks.py
+++ prac 4/quick_picks.py
@@ -6,24 +6,24 @@ Quick pick program
 import random
 
 NUMBERS_PER_LINE = 6
-MINIMUM = 1
-MAXIMUM = 45
+MIN = 1
+MAX = 45
 
 
 def main():
     """Quick picks program - choose sets of random numbers."""
-    number_of_quick_picks = int(input("How many quick picks? "))
+    number_of_quick_picks = int(input("How many number you picks? "))
     while number_of_quick_picks < 0:
-        print("That makes no sense!")
-        number_of_quick_picks = int(input("How many quick picks? "))
+        print("Invalid number!")
+        number_of_quick_picks = int(input("How many number you picks? "))
 
     for i in range(number_of_quick_picks):
         quick_pick = []
-        for j in range(NUMBERS_PER_LINE):
-            number = random.randint(MINIMUM, MAXIMUM)
-            while number in quick_pick:
-                number = random.randint(MINIMUM, MAXIMUM)
-            quick_pick.append(number)
+        for o in range(NUMBERS_PER_LINE):
+            numbers = random.randint(MIN, MAX)
+            while numbers in quick_pick:
+                numbers = random.randint(MIN, MAX)
+            quick_pick.append(numbers)
         quick_pick.sort()
         # the following uses a generator expression (like a list comprehension)
         # to format each number in quick_pick in the same way
